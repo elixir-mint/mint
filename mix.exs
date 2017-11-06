f395b86fee115ea22dd8412ea32f42f962df4c8d
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2017-11-06 10:04:35 +0100

    Add travis

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule XHTTP.MixProject do
     [
       app: :xhttp,
       version: "0.1.0",
-      elixir: "~> 1.6-dev",
+      elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
