880a72044714ffd8294881e6c40c74f7ec4844a3
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2017-11-08 11:58:17 +0100

    Add pipelined property test

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -10,12 +10,32 @@ defmodule XHTTP.TestHelpers do
     merge_body(responses, request, body <> new_body)
   end
 
-  defp merge_body([{:headers, request, trailing}, {:done, request}], request, body) do
+  defp merge_body([{:done, request}], request, body) do
+    body
+  end
+
+  def merge_body_with_trailers(responses, request) do
+    merge_body_with_trailers(responses, request, "")
+  end
+
+  defp merge_body_with_trailers([{:body, request, new_body} | responses], request, body) do
+    merge_body_with_trailers(responses, request, body <> new_body)
+  end
+
+  defp merge_body_with_trailers([{:headers, request, trailing}, {:done, request}], request, body) do
     {body, trailing}
   end
 
-  defp merge_body([{:done, request}], request, body) do
-    body
+  def merge_pipelined_body(responses, request) do
+    merge_pipelined_body(responses, request, "")
+  end
+
+  defp merge_pipelined_body([{:body, request, new_body} | responses], request, body) do
+    merge_pipelined_body(responses, request, body <> new_body)
+  end
+
+  defp merge_pipelined_body([{:done, request} | rest], request, body) do
+    {body, rest}
   end
 end
 
--- test/xhttp/conn_properties_test.exs
+++ test/xhttp/conn_properties_test.exs
@@ -44,9 +44,33 @@ defmodule XHTTP.ConnPropertiesTest do
       assert [status, headers | rest] = responses
       assert {:status, ^ref, {{1, 1}, 200, "OK"}} = status
       assert {:headers, ^ref, [{"transfer-encoding", "chunked"}]} = headers
-      assert merge_body(rest, ref) == {"0123", [{"trailer", "value"}]}
+      assert merge_body_with_trailers(rest, ref) == {"0123", [{"trailer", "value"}]}
 
       assert conn.buffer == "XXX"
+    end
+  end
+
+  property "pipeline with multiple responses in single message" do
+    {:ok, conn} = Conn.connect("localhost", 80, transport: TCPMock)
+    {:ok, conn, ref1} = Conn.request(conn, "GET", "/", [], nil)
+    {:ok, conn, ref2} = Conn.request(conn, "GET", "/", [], nil)
+    {:ok, conn, ref3} = Conn.request(conn, "GET", "/", [], nil)
+    response = "HTTP/1.1 200 OK\r\ncontent-length: 5\r\n\r\nXXXXX"
+    responses = for _ <- 1..3, do: response, into: ""
+
+    check all byte_chunks <- random_chunks(responses) do
+      {_conn, responses} =
+        Enum.reduce(byte_chunks, {conn, []}, fn bytes, {conn, responses} ->
+          assert {:ok, conn, new_responses} = Conn.stream(conn, {:tcp, conn.socket, bytes})
+          {conn, responses ++ new_responses}
+        end)
+
+      assert [{:status, ^ref1, _}, {:headers, ^ref1, _} | responses] = responses
+      assert {"XXXXX", responses} = merge_pipelined_body(responses, ref1)
+      assert [{:status, ^ref2, _}, {:headers, ^ref2, _} | responses] = responses
+      assert {"XXXXX", responses} = merge_pipelined_body(responses, ref2)
+      assert [{:status, ^ref3, _}, {:headers, ^ref3, _} | responses] = responses
+      assert {"XXXXX", []} = merge_pipelined_body(responses, ref3)
     end
   end
 
@@ -54,15 +78,15 @@ defmodule XHTTP.ConnPropertiesTest do
     size = byte_size(binary)
 
     StreamData.bind(StreamData.integer(0..size), fn num_splits ->
-      StreamData.integer(1..size-1)
+      StreamData.integer(1..(size - 1))
       |> Enum.take(num_splits)
       |> Enum.uniq()
       |> Enum.sort()
       |> Enum.reduce({[], binary, 0}, fn split, {chunks, rest, prev_split} ->
-        length = split - prev_split
-        <<chunk::binary-size(length), rest::binary>> = rest
-        {[chunk | chunks], rest, split}
-      end)
+           length = split - prev_split
+           <<chunk::binary-size(length), rest::binary>> = rest
+           {[chunk | chunks], rest, split}
+         end)
       |> join_last_chunk()
       |> Enum.reverse()
       |> StreamData.constant()
