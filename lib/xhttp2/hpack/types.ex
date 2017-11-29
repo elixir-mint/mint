cb3f02fb9deac66abb1abb60d876e7a6a4aefd57
Andrea Leopardi <an.leopardi@gmail.com>
2017-11-29 23:01:13 +0100

    Add HPACK types encoding/decoding

---
+++ lib/xhttp2/hpack/types.ex
@@ -0,0 +1,75 @@
+defmodule XHTTP2.HPACK.Types do
+  @moduledoc false
+
+  use Bitwise
+
+  defmacrop power_of_two(n) do
+    quote do
+      1 <<< unquote(n)
+    end
+  end
+
+  @spec encode_integer(non_neg_integer(), 1..8) :: bitstring()
+  def encode_integer(integer, prefix)
+
+  def encode_integer(integer, prefix) when integer < power_of_two(prefix) - 1 do
+    <<integer::size(prefix)>>
+  end
+
+  def encode_integer(integer, prefix) do
+    initial = power_of_two(prefix) - 1
+    remaining = integer - initial
+    <<initial::size(prefix), encode_remaining_integer(remaining)::binary>>
+  end
+
+  defp encode_remaining_integer(remaining) when remaining >= 128 do
+    first = rem(remaining, 128) + 128
+    <<first::8, encode_remaining_integer(div(remaining, 128))::binary>>
+  end
+
+  defp encode_remaining_integer(remaining) do
+    <<remaining::8>>
+  end
+
+  @spec encode_binary(binary(), boolean()) :: binary()
+  def encode_binary(binary, huffman?) do
+    binary = if huffman?, do: encode_huffman(binary), else: binary
+    huffman_bit = if huffman?, do: 1, else: 0
+    binary_size = encode_integer(byte_size(binary), 7)
+    <<huffman_bit::1, binary_size::bitstring, binary::binary>>
+  end
+
+  @spec decode_integer(bitstring, 1..8) :: {non_neg_integer(), binary()}
+  def decode_integer(bitstring, prefix) do
+    <<value::size(prefix), rest::binary>> = bitstring
+
+    if value < power_of_two(prefix) - 1 do
+      {value, rest}
+    else
+      decode_remaining_integer(rest, value, 0)
+    end
+  end
+
+  defp decode_remaining_integer(<<0::1, value::7, rest::binary>>, int, m) do
+    {int + (value <<< m), rest}
+  end
+
+  defp decode_remaining_integer(<<1::1, value::7, rest::binary>>, int, m) do
+    decode_remaining_integer(rest, int + (value <<< m), m + 7)
+  end
+
+  @spec decode_binary(binary) :: {binary(), binary()}
+  def decode_binary(binary) do
+    <<huffman_bit::1, rest::bitstring>> = binary
+    {length, rest} = decode_integer(rest, 7)
+    <<contents::binary-size(length), rest::binary>> = rest
+
+    contents = if huffman_bit, do: decode_huffman(contents), else: contents
+
+    {contents, rest}
+  end
+
+  # TODO: Huffman encoding
+  defp encode_huffman(binary), do: binary
+  defp decode_huffman(binary), do: binary
+end
---
+++ test/xhttp2/hpack/types_test.exs
@@ -0,0 +1,35 @@
+defmodule XHTTP2.HPACK.TypesTest do
+  use ExUnit.Case, async: true
+  use ExUnitProperties
+
+  import XHTTP2.HPACK.Types
+
+  test "encode_integer/2 with examples from the spec" do
+    assert encode_integer(10, _prefix = 5) == <<0b01010::5>>
+    assert encode_integer(1337, 5) == <<0b11111_10011010_00001010::21>>
+    assert encode_integer(42, 8) == <<0b00101010::8>>
+  end
+
+  test "decode_integer/2 with examples from the spec" do
+    assert decode_integer(<<0b01010::5, "foo">>, _prefix = 5) == {10, "foo"}
+    assert decode_integer(<<0b11111_10011010_00001010::21, "foo">>, 5) == {1337, "foo"}
+    assert decode_integer(<<0b00101010::8, "foo">>, 8) == {42, "foo"}
+  end
+
+  property "encoding and then decoding integers is circular" do
+    check all value <- map(integer(), &abs/1),
+              prefix <- integer(1..8),
+              cruft <- binary() do
+      encoded = encode_integer(value, prefix)
+      assert decode_integer(<<encoded::bitstring, cruft::binary>>, prefix) == {value, cruft}
+    end
+  end
+
+  property "encoding and then decoding strings is circular" do
+    check all string <- binary(),
+              cruft <- binary() do
+      encoded = encode_binary(string, _huffman? = false)
+      assert decode_binary(<<encoded::binary, cruft::binary>>) == {string, cruft}
+    end
+  end
+end
