dca16e6a07f729c480fc8363c063000958b585da
Andrea Leopardi <an.leopardi@gmail.com>
2017-11-30 10:44:38 +0100

    Update stream_data to the latest release

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule XHTTP.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:stream_data, "~> 0.3.0", only: :test}
+      {:stream_data, "~> 0.4.0", only: :test}
     ]
   end
 end
