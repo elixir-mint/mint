f582b6d779c036e0e184befc485617ad55781913
Andrea Leopardi <an.leopardi@gmail.com>
2017-12-01 10:57:28 +0100

    Restructure HPACK.Huffman

--- lib/hpack/huffman.ex
+++ lib/hpack/huffman.ex
@@ -8,7 +8,27 @@ defmodule HPACK.Huffman do
   table_file = Path.absname("huffman_table", __DIR__)
   @external_resource table_file
 
-  @eos 256
+  entries =
+    Enum.map(File.stream!(table_file), fn line ->
+      [byte_value, bits, _hex, bit_count] =
+        line
+        |> case do
+             <<?', _, ?', ?\s, rest::binary>> -> rest
+             "EOS " <> rest -> rest
+             _other -> line
+           end
+        |> String.replace(["|", "(", ")", "[", "]"], "")
+        |> String.split()
+
+      byte_value = String.to_integer(byte_value)
+      bits = String.to_integer(bits, 2)
+      bit_count = String.to_integer(bit_count)
+
+      {byte_value, bits, bit_count}
+    end)
+
+  {regular_entries, [eos_entry]} = Enum.split(entries, -1)
+  {_eos_byte_value, eos_bits, eos_bit_count} = eos_entry
 
   defmacrop take_significant_bits(value, bit_count, bits_to_take) do
     quote do
@@ -16,70 +36,54 @@ defmodule HPACK.Huffman do
     end
   end
 
+  ## Encoding
+
   def encode(binary) do
     encode(binary, _acc = <<>>)
   end
 
-  for line <- File.stream!(table_file) do
-    [byte_value, bits, _hex, bit_count] =
-      line
-      |> case do
-           <<?', _, ?', ?\s, rest::binary>> -> rest
-           "EOS " <> rest -> rest
-           _other -> line
-         end
-      |> String.replace(["|", "(", ")", "[", "]"], "")
-      |> String.split()
+  for {byte_value, bits, bit_count} <- regular_entries do
+    defp encode(<<unquote(byte_value), rest::binary>>, acc) do
+      encode(rest, <<acc::bitstring, unquote(bits)::size(unquote(bit_count))>>)
+    end
+  end
 
-    byte_value = String.to_integer(byte_value)
-    bits = String.to_integer(bits, 2)
-    bit_count = String.to_integer(bit_count)
+  defp encode(<<>>, acc) do
+    overflowing_bits = rem(bit_size(acc), 8)
 
-    if byte_value == @eos do
-      def encode(<<>>, acc) do
-        overflowing_bits = rem(bit_size(acc), 8)
-
-        if overflowing_bits == 0 do
-          acc
-        else
-          bits_to_add = 8 - overflowing_bits
-
-          value_of_bits_to_add =
-            take_significant_bits(unquote(bits), unquote(bit_count), bits_to_add)
-
-          res = <<acc::bitstring, value_of_bits_to_add::size(bits_to_add)>>
-
-          if not is_binary(res) do
-            raise inspect(res)
-          end
-
-          res
-        end
-      end
-
-      def decode(<<>>) do
-        <<>>
-      end
-
-      # Use binary syntax for single match context optimization.
-      def decode(<<padding::bitstring>>) when bit_size(padding) in 1..7 do
-        padding_size = bit_size(padding)
-        <<padding::size(padding_size)>> = padding
-
-        if take_significant_bits(unquote(bits), unquote(bit_count), padding_size) == padding do
-          <<>>
-        else
-          raise "decoding error"
-        end
-      end
+    if overflowing_bits == 0 do
+      acc
     else
-      def encode(<<unquote(byte_value), rest::binary>>, acc) do
-        encode(rest, <<acc::bitstring, unquote(bits)::size(unquote(bit_count))>>)
-      end
+      bits_to_add = 8 - overflowing_bits
 
-      def decode(<<unquote(bits)::size(unquote(bit_count)), rest::bitstring>>) do
-        <<unquote(byte_value), decode(rest)::binary>>
-      end
+      value_of_bits_to_add =
+        take_significant_bits(unquote(eos_bits), unquote(eos_bit_count), bits_to_add)
+
+      <<acc::bitstring, value_of_bits_to_add::size(bits_to_add)>>
+    end
+  end
+
+  ## Decoding
+
+  for {byte_value, bits, bit_count} <- regular_entries do
+    def decode(<<unquote(bits)::size(unquote(bit_count)), rest::bitstring>>) do
+      <<unquote(byte_value), decode(rest)::binary>>
+    end
+  end
+
+  def decode(<<>>) do
+    <<>>
+  end
+
+  # Use binary syntax for single match context optimization.
+  def decode(<<padding::bitstring>>) when bit_size(padding) in 1..7 do
+    padding_size = bit_size(padding)
+    <<padding::size(padding_size)>> = padding
+
+    if take_significant_bits(unquote(eos_bits), unquote(eos_bit_count), padding_size) == padding do
+      <<>>
+    else
+      raise "decoding error"
     end
   end
 end
