005f001933564ffd37ed8bb32c795e4fd88c29a4
Andrea Leopardi <an.leopardi@gmail.com>
2017-12-17 12:37:44 +0100

    WIP Support different transports

--- lib/xhttp2/conn.ex
+++ lib/xhttp2/conn.ex
@@ -8,6 +8,7 @@ defmodule XHTTP2.Conn do
   require Logger
 
   defstruct [
+    :transport,
     :socket,
     :state,
     :server_settings,
@@ -35,14 +36,17 @@ defmodule XHTTP2.Conn do
   ## Public interface
 
   def connect(hostname, port, opts \\ []) do
+    transport = Keyword.get(opts, :transport, :ssl)
+
     transport_opts =
       opts
       |> Keyword.get(:transport_opts, [])
       |> Keyword.merge(@forced_transport_opts)
 
-    with {:ok, socket} <- connect_and_negotiate_protocol(hostname, port, transport_opts),
-         :ok <- set_inet_opts(socket),
-         {:ok, conn} <- initiate_connection(socket, opts) do
+    with {:ok, socket} <-
+           connect_and_negotiate_protocol(hostname, port, transport, transport_opts),
+         :ok <- set_inet_opts(transport, socket),
+         {:ok, conn} <- initiate_connection(transport, socket, opts) do
       {:ok, conn}
     end
   end
@@ -51,9 +55,9 @@ defmodule XHTTP2.Conn do
 
   ## Helpers
 
-  defp connect_and_negotiate_protocol(hostname, port, transport_opts) do
-    with {:ok, socket} <- :ssl.connect(String.to_charlist(hostname), port, transport_opts),
-         {:ok, protocol} <- :ssl.negotiated_protocol(socket) do
+  defp connect_and_negotiate_protocol(hostname, port, transport, transport_opts) do
+    with {:ok, socket} <- transport.connect(String.to_charlist(hostname), port, transport_opts),
+         {:ok, protocol} <- transport.negotiated_protocol(socket) do
       if protocol == "h2" do
         {:ok, socket}
       else
@@ -62,19 +66,25 @@ defmodule XHTTP2.Conn do
     end
   end
 
-  defp set_inet_opts(socket) do
-    with {:ok, opts} <- :ssl.getopts(socket, [:sndbuf, :recbuf, :buffer]),
-         buffer = Keyword.fetch!(opts, :buffer),
-         sndbuf = Keyword.fetch!(opts, :sndbuf),
-         recbuf = Keyword.fetch!(opts, :recbuf),
-         buffer = buffer |> max(sndbuf) |> max(recbuf),
-         :ok <- :ssl.setopts(socket, buffer: buffer),
-         do: :ok
+  defp set_inet_opts(transport, socket) do
+    inet = transport_to_inet(transport)
+
+    with {:ok, opts} <- inet.getopts(socket, [:sndbuf, :recbuf, :buffer]) do
+      buffer =
+        Keyword.fetch!(opts, :buffer)
+        |> max(Keyword.fetch!(opts, :sndbuf))
+        |> max(Keyword.fetch!(opts, :recbuf))
+
+      inet.setopts(socket, buffer: buffer)
+    end
   end
+
+  defp transport_to_inet(:gen_tcp), do: :inet
+  defp transport_to_inet(other), do: other
 
   # http://httpwg.org/specs/rfc7540.html#rfc.section.6.5
   # SETTINGS parameters are not negotiated. We keep client settings and server settings separate.
-  defp initiate_connection(socket, opts) do
+  defp initiate_connection(transport, socket, opts) do
     client_settings_params =
       Keyword.merge(@default_client_settings, Keyword.get(opts, :client_settings, []))
 
@@ -87,13 +97,14 @@ defmodule XHTTP2.Conn do
         flags: set_flag(:frame_settings, :ack)
       )
 
-    with :ok <- :ssl.send(socket, [@connection_preface, Frame.encode(client_settings)]),
-         {:ok, server_settings, buffer} <- receive_server_settings(socket),
-         :ok <- :ssl.send(socket, Frame.encode(server_settings_ack)),
-         {:ok, buffer} <- receive_client_settings_ack(socket, buffer),
-         :ok <- :ssl.setopts(socket, active: true) do
+    with :ok <- transport.send(socket, [@connection_preface, Frame.encode(client_settings)]),
+         {:ok, server_settings, buffer} <- receive_server_settings(transport, socket),
+         :ok <- transport.send(socket, Frame.encode(server_settings_ack)),
+         {:ok, buffer} <- receive_client_settings_ack(transport, socket, buffer),
+         :ok <- transport_to_inet(transport).setopts(socket, active: true) do
       conn = %__MODULE__{
         state: :open,
+        transport: transport,
         socket: socket,
         buffer: buffer,
         client_settings: client_settings_params,
@@ -104,16 +115,16 @@ defmodule XHTTP2.Conn do
     end
   end
 
-  defp receive_server_settings(socket) do
-    case recv_next_frame(socket, _buffer = "") do
+  defp receive_server_settings(transport, socket) do
+    case recv_next_frame(transport, socket, _buffer = "") do
       {:ok, frame_settings(), _buffer} = result -> result
       {:ok, _frame, _buffer} -> {:error, :protocol_error}
       {:error, _reason} = error -> error
     end
   end
 
-  defp receive_client_settings_ack(socket, buffer) do
-    case recv_next_frame(socket, buffer) do
+  defp receive_client_settings_ack(transport, socket, buffer) do
+    case recv_next_frame(transport, socket, buffer) do
       {:ok, frame_settings(flags: flags), buffer} ->
         if flag_set?(flags, :frame_settings, :ack) do
           {:ok, buffer}
@@ -127,7 +138,7 @@ defmodule XHTTP2.Conn do
           "Received a WINDOW_UPDATE while waiting for client SETTINGS ack: #{inspect(frame)}"
         end)
 
-        receive_client_settings_ack(socket, buffer)
+        receive_client_settings_ack(transport, socket, buffer)
 
       {:ok, frame_goaway() = frame, _buffer} ->
         {:error, {:goaway, frame}}
@@ -137,14 +148,14 @@ defmodule XHTTP2.Conn do
     end
   end
 
-  defp recv_next_frame(socket, buffer) do
+  defp recv_next_frame(transport, socket, buffer) do
     case Frame.decode_next(buffer) do
       {:ok, _frame, _rest} = result ->
         result
 
       {:error, {:malformed_frame, _}} ->
-        with {:ok, data} <- :ssl.recv(socket, 0) do
-          recv_next_frame(socket, buffer <> data)
+        with {:ok, data} <- transport.recv(socket, 0) do
+          recv_next_frame(transport, socket, buffer <> data)
         end
 
       {:error, _reason} = error ->
