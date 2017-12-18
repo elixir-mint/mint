5b6445593d5c6c0330a4f1bf0902e42b6e02c349
Andrea Leopardi <an.leopardi@gmail.com>
2017-12-18 11:28:22 +0100

    Improve tests

--- test/xhttp2/integration_test.exs
+++ test/xhttp2/integration_test.exs
@@ -20,14 +20,18 @@ defmodule XHTTP2.IntegrationTest do
     @moduletag connect: {"http2.golang.org", 443}
 
     test "GET /reqinfo", %{conn: conn} do
-      assert {:ok, %Conn{} = conn, req_id} =
-               Conn.request(conn, headers_for_request("GET", "https://http2.golang.org/reqinfo"))
+      headers = headers_for_request("GET", "https://http2.golang.org/reqinfo")
+      assert {:ok, %Conn{} = conn, req_id} = Conn.request(conn, headers)
 
-      assert {:ok, %Conn{} = conn, responses} = stream_message(conn)
-      assert [{:headers, ^req_id, _headers}] = responses
+      assert {:ok, %Conn{} = conn, responses} = receive_stream(conn)
 
-      assert {:ok, %Conn{} = conn, responses} = stream_message(conn)
-      assert [{:data, ^req_id, data}, {:done, ^req_id}] = responses
+      assert [
+               {:headers, ^req_id, headers},
+               {:data, ^req_id, data},
+               {:done, ^req_id}
+             ] = responses
+
+      assert is_list(headers)
       assert data =~ "Method: GET"
 
       assert conn.buffer == ""
@@ -36,18 +40,19 @@ defmodule XHTTP2.IntegrationTest do
 
     test "PUT /ECHO", %{conn: conn} do
       headers = headers_for_request("PUT", "https://http2.golang.org/ECHO")
-
       assert {:ok, %Conn{} = conn, req_id} = Conn.request(conn, headers, "hello world")
 
-      # TODO: this is a WINDOW_UPDATE. Figure out how to not care about this in the test.
-      assert {:ok, %Conn{} = conn, []} = stream_message(conn)
+      assert {:ok, %Conn{} = conn, responses} = receive_stream(conn)
 
-      assert {:ok, %Conn{} = conn, responses} = stream_message(conn)
-      assert [{:headers, ^req_id, _headers}, {:data, ^req_id, data}] = responses
+      assert [
+               {:headers, ^req_id, headers},
+               {:data, ^req_id, data},
+               {:data, ^req_id, ""},
+               {:done, ^req_id}
+             ] = responses
+
+      assert is_list(headers)
       assert data == "HELLO WORLD"
-
-      assert {:ok, %Conn{} = conn, responses} = stream_message(conn)
-      assert [{:data, ^req_id, ""}, {:done, ^req_id}] = responses
 
       assert conn.buffer == ""
       assert Conn.open?(conn)
@@ -55,7 +60,7 @@ defmodule XHTTP2.IntegrationTest do
 
     test "ping", %{conn: conn} do
       assert {:ok, %Conn{} = conn} = Conn.ping(conn)
-      assert {:ok, %Conn{} = conn, [:pong]} = stream_message(conn)
+      assert {:ok, %Conn{} = conn, [:pong]} = receive_stream(conn)
       assert conn.buffer == ""
       assert Conn.open?(conn)
     end
@@ -72,11 +77,51 @@ defmodule XHTTP2.IntegrationTest do
     ]
   end
 
-  defp stream_message(conn) do
+  defp receive_stream(conn) do
+    receive_stream(conn, [])
+  end
+
+  defp receive_stream(conn, responses) do
     receive do
-      message -> Conn.stream(conn, message)
+      {:rest, conn, rest_responses} ->
+        maybe_done(conn, rest_responses, responses)
+
+      {tag, _socket, _data} = message when tag in [:tcp, :ssl] ->
+        assert {:ok, %Conn{} = conn, new_responses} = Conn.stream(conn, message)
+        maybe_done(conn, new_responses, responses)
+
+      {tag, _socket} = message when tag in [:tcp_close, :ssl_close] ->
+        assert {:error, %Conn{}, :closed} = Conn.stream(conn, message)
+
+      {tag, _reason} = message when tag in [:tcp_error, :ssl_error] ->
+        assert {:error, %Conn{}, _reason} = Conn.stream(conn, message)
     after
-      10_000 -> flunk("did not receive any message after 10s")
+      10000 ->
+        flunk("receive_stream timeout")
     end
+  end
+
+  defp maybe_done(conn, [{:done, _} = done | rest], acc) do
+    if rest != [] do
+      send(self(), {:rest, conn, rest})
+    end
+
+    {:ok, conn, acc ++ [done]}
+  end
+
+  defp maybe_done(conn, [:pong | rest], acc) do
+    if rest != [] do
+      send(self(), {:rest, conn, rest})
+    end
+
+    {:ok, conn, acc ++ [:pong]}
+  end
+
+  defp maybe_done(conn, [resp | rest], acc) do
+    maybe_done(conn, rest, acc ++ [resp])
+  end
+
+  defp maybe_done(conn, [], acc) do
+    receive_stream(conn, acc)
   end
 end
