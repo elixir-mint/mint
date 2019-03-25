4b609b1a629c8926f7ff47c3451995b0e3fe2344
Andrea Leopardi <an.leopardi@gmail.com>
2019-03-25 15:39:32 +0100

    FIXUP

--- mix.exs
+++ mix.exs
@@ -15,8 +15,7 @@ defmodule Mint.MixProject do
 
       # Dialyxir
       dialyzer: [
-        plt_add_apps: [:castore, :inets, :public_key, :ssl],
-        plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
+        plt_add_apps: [:castore, :inets]
       ],
 
       # Hex
@@ -36,7 +35,7 @@ defmodule Mint.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :ssl]
     ]
   end
 
