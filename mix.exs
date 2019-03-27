d12f00cb3263847c24d5e01e90c49a7c13889b38
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2019-03-27 08:44:57 -0700

    Remove dependency on inets (#156)

--- mix.exs
+++ mix.exs
@@ -35,7 +35,7 @@ defmodule Mint.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :ssl, :inets]
+      extra_applications: [:logger, :ssl]
     ]
   end
 
