72fa1b296cc46482520e08c43c7fd5f433eb5d5e
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2020-09-22 12:29:33 +0200

    Add check for :persistent_term.get/2

--- lib/mint/application.ex
+++ lib/mint/application.ex
@@ -3,7 +3,10 @@ defmodule Mint.Application do
   use Application
 
   def start(_type, _args) do
-    Application.put_env(:mint, :persistent_term, Code.ensure_loaded?(:persistent_term))
+    persistent_term =
+      Code.ensure_loaded?(:persistent_term) and function_exported?(:persistent_term, :get, 2)
+
+    Application.put_env(:mint, :persistent_term, persistent_term)
 
     opts = [strategy: :one_for_one, name: Mint.Supervisor]
     Supervisor.start_link([], opts)
