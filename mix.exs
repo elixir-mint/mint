c4f0ed891093ea11008701e0a57129db331fbd3e
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2020-09-23 11:07:22 +0200

    Cache the :cacertfile option (#275)

--- mix.exs
+++ mix.exs
@@ -13,6 +13,13 @@ defmodule Mint.MixProject do
       elixirc_paths: elixirc_paths(Mix.env()),
       deps: deps(),
 
+      # Xref
+      xref: [
+        exclude: [
+          :persistent_term
+        ]
+      ],
+
       # Dialyxir
       dialyzer: [
         plt_add_apps: [:castore]
@@ -38,7 +45,8 @@ defmodule Mint.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :ssl]
+      extra_applications: [:logger, :ssl],
+      mod: {Mint.Application, []}
     ]
   end
 
