5c0cb5fff87a7466685bb9af4f8e95e2850bf5a1
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2026-09-24 23:29:42 +0200

    Leave the zone ID out of bracketed IPv6 literal hosts
    :inet.parse_ipv6strict_address/1 accepts a zone ID, so "fe80::1%eth0"
    was sent as "host: [fe80::1%eth0]:8080". The IP-literal grammar of
    RFC 3986 3.2.2 that the Host header and :authority use has no zone ID,
    so only the address is sent.

--- test/mint/http1/conn_test.exs
+++ test/mint/http1/conn_test.exs
@@ -796,6 +796,17 @@ defmodule Mint.HTTP1Test do
              """)
   end
 
+  test "the zone ID of an IPv6 literal hostname is left out of the host header",
+       %{port: port, server_ref: server_ref} do
+    for hostname <- ["fe80::1%eth0", "fe80::1%25eth0"] do
+      assert {:ok, conn} = HTTP1.connect(:http, "localhost", port, hostname: hostname)
+      assert_receive {^server_ref, server_socket}
+      {:ok, _conn, _ref} = HTTP1.request(conn, "GET", "/", [], nil)
+
+      assert receive_request_string(server_socket) =~ "\r\nhost: [fe80::1]:#{port}\r\n"
+    end
+  end
+
   test "open_request_count/1", %{conn: conn} do
     assert HTTP1.open_request_count(conn) == 0
 
